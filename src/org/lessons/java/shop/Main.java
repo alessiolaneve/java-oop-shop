095cd849c1a3d7763c2b806ac4cac38da6a1c9d2
Alessio La Neve <90204732+alessiolaneve@users.noreply.github.com>
2024-05-15 19:11:04 +0200

    prime consegne

---
+++ src/org/lessons/java/shop/Main.java
@@ -0,0 +1,12 @@
+package org.lessons.java.shop;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Prodotto prodotto = new Prodotto();
+		System.out.println(prodotto.getCodice());
+	}
+		
+}
+
+
---
+++ src/org/lessons/java/shop/Prodotto.java
@@ -0,0 +1,82 @@
+package org.lessons.java.shop;
+
+
+/*Un prodotto è caratterizzato da: - codice (numero intero) - nome - descrizione - prezzo - iva
+Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter ed eventuali altri metodi 
+di “utilità” per fare in modo che: 
+
+- gli altri attributi siano accessibili sia in lettura che in scrittura 
+- il prodotto esponga un metodo per avere il prezzo base 
+- il prodotto esponga un metodo per avere il prezzo comprensivo di iva 
+- il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice-nome 
+Nello stesso package aggiungete una classe Main con metodo main nella quale testate tutte le funzionalità della classe Prodotto.
+BONUS: create un metodo (statico) che restituisca il codice con un pad left di 0 per arrivare a 8 caratteri 
+(ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)*/
+
+public class Prodotto {
+	private String codice;
+	private String nome;
+	private String descrizione;
+	private double prezzo;
+	private double iva;
+
+	// costruttore
+	public Prodotto() {
+		this.codice = generaCodice();
+		this.nome = nome;
+		this.descrizione = descrizione;
+		this.prezzo = prezzo;
+		this.iva = iva;
+	}
+	
+	// - alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random 
+
+	private String generaCodice() {
+		
+		double random = Math.random();
+		return String.valueOf(random);
+		}
+
+	// - il codice prodotto sia accessibile solo in lettura 
+	// - gli altri attributi siano accessibili sia in lettura che in scrittura 
+
+	public String getCodice() {
+		return codice;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public double getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+
+	public double getIva() {
+		return iva;
+	}
+
+	public void setIva(double iva) {
+		this.iva = iva;
+	}
+}
+
+
+
+	
