a904a9bfd09e5236e11722980a920d2c76b33635
Alessio La Neve <90204732+alessiolaneve@users.noreply.github.com>
2024-05-15 19:44:06 +0200

    dati input

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,10 +1,24 @@
 package org.lessons.java.shop;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		Prodotto prodotto = new Prodotto();
 		System.out.println(prodotto.getCodice());
+		Scanner scan = new Scanner(System.in);
+		
+		// chiedo nome e prezzo del prodotto all'utente
+		System.out.println("Inserisci i dati del nuovo prodotto.");
+		System.out.println("Nome :");
+		String nome = scan.nextLine();
+		
+		System.out.println("Prezzo (Inserisci un numero con la virgola e due cifre decimali):");
+		Double prezzo = scan.nextDouble();
+		
+		System.out.println(nome);
+		System.out.println(prezzo);
 	}
 		
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -5,8 +5,6 @@ package org.lessons.java.shop;
 Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter ed eventuali altri metodi 
 di “utilità” per fare in modo che: 
 
-- gli altri attributi siano accessibili sia in lettura che in scrittura 
-- il prodotto esponga un metodo per avere il prezzo base 
 - il prodotto esponga un metodo per avere il prezzo comprensivo di iva 
 - il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice-nome 
 Nello stesso package aggiungete una classe Main con metodo main nella quale testate tutte le funzionalità della classe Prodotto.
@@ -23,10 +21,12 @@ public class Prodotto {
 	// costruttore
 	public Prodotto() {
 		this.codice = generaCodice();
+		/* li prendo in input dall'utente tramite metodi
 		this.nome = nome;
 		this.descrizione = descrizione;
 		this.prezzo = prezzo;
 		this.iva = iva;
+		*/		
 	}
 	
 	// - alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random 
@@ -36,6 +36,12 @@ public class Prodotto {
 		double random = Math.random();
 		return String.valueOf(random);
 		}
+	
+	// - il prodotto esponga un metodo per avere il prezzo base 
+	public void stampaPrezzo() {
+		String prezzoString = prezzo + "";
+		System.out.println("Il prezzo base del prodotto è: " + prezzoString);
+	}
 
 	// - il codice prodotto sia accessibile solo in lettura 
 	// - gli altri attributi siano accessibili sia in lettura che in scrittura 
